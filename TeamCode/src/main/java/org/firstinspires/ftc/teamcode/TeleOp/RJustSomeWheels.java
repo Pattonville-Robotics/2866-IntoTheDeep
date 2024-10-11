af25c5f96ac575e9613887821ffd84149553e0e0
iiAzui <rhys.harden@gmail.com>
2024-10-11 13:21:01 -0500

    add triggers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RJustSomeWheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RJustSomeWheels.java
@@ -12,7 +12,6 @@ public class RJustSomeWheels extends LinearOpMode{
         DcMotor motorBackLeft = hardwareMap.dcMotor.get("backLeft");
         DcMotor motorFrontLeft = hardwareMap.dcMotor.get("frontLeft");
 
-
         motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
         motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
@@ -20,12 +19,20 @@ public class RJustSomeWheels extends LinearOpMode{
             double y = gamepad1.left_stick_y; // Remember, this is reversed!
             double x = -gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
+            double speed;
 
+            if (gamepad1.right_trigger>0) {
+                speed = 0.8;
+            } else if (gamepad1.left_trigger>0){
+                speed = 0.2;
+            } else {
+                speed = 0.4;
+            }
             double den = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (Math.pow((y + x + rx),3) * 0.4) / den;
-            double backLeftPower = (Math.pow((y - x + rx),3) * 0.4) / den;
-            double frontRightPower = (Math.pow((y - x - rx),3) * 0.4) / den;
-            double backRightPower = -(Math.pow((y + x - rx),3) * 0.4) / den;
+            double frontLeftPower = (Math.pow((y + x + rx),3) * speed) / den;
+            double backLeftPower = (Math.pow((y - x + rx),3) * speed) / den;
+            double frontRightPower = (Math.pow((y - x - rx),3) * speed) / den;
+            double backRightPower = -(Math.pow((y + x - rx),3) * speed) / den;
 
             motorFrontLeft.setPower(frontLeftPower);
             motorBackLeft.setPower(backLeftPower);
